baee84b01ab38904eb68c9da2fe45beadec50011
Hermann Rasch <hermann.rasch93@gmail.com>
2023-04-28 00:40:06 +0200

    Test scenarios for the Number of Events

---
+++ src/test/NumberOfEvents.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockData } from '../MockData';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEventsWrapper /> component', () => {
+    let NumberOfEventsWrapper;
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    });
+
+    test('test render text input', () => {
+        expect(NumberOfEventsWrapper.find('.NumberOfEvents')).toHaveLength(1);
+    });
+
+    test('test change number of events', () => {
+        NumberOfEventsWrapper.setState({
+            numberOfEvents : '32' })
+        const eventObject = { target: {value: '15'}};
+        NumberOfEventsWrapper.find('.selectedNumberEvents').simulate('change', eventObject);
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(15);
+    });
+
+    test('test default number of events', () => {
+        expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+    })
+});
