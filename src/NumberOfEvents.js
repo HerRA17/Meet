51f91ea96c84302abc5eadf91b11f60fac90ea86
Hermann Rasch <hermann.rasch93@gmail.com>
2023-05-04 19:11:27 +0200

    Formatting & conditional to the function handle input change

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,23 +3,27 @@ class NumberOfEvents extends Component{
     constructor() {
         super();
         this.state = {
-            numberOfEvents: 32
-        }
-
-        this.handleInputChanged = (eventamout) => {
-            const value = eventamout.target.value; 
-            this.setState({
-                numberOfEvents: value,
-            })
+            eventNumberResult: 32
         }
     }
+
+    handleInputChanged = (eventNumberResult) => {
+        const value = eventNumberResult.target.value; 
+        if (value >= 1 || value <= 32) {
+        this.setState({
+            eventNumberResult: value,
+        })
+        this.props.updateEvents(this.props.selectedCity, value)    
+    }
+    }
+
 render() {
     return(
         <div className='NumberOfEvents'>
             <input 
             input='number'
-             className='selectedNumberEvents'
-             value={this.state.numberOfEvents}
+             className='selectedEventNumberResult'
+             value={this.state.eventNumberResult}
              onChange={this.handleInputChanged}
              placeholder='Enter the amount of events'/>
         </div>
