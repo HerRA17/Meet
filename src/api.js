bd75d23eac16374f09d16a120cb2d6ce6eae6544
Hermann Rasch <hermann.rasch93@gmail.com>
2023-05-07 17:07:21 +0200

    Correction of a getAccessToken Function

--- src/api.js
+++ src/api.js
@@ -1,11 +1,6 @@
-/** 
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
 import { mockData } from "./MockData";
-import axios from "axios";
 import NProgress from "nprogress";
+import axios from "axios";
 
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
@@ -13,22 +8,24 @@ export const extractLocations = (events) => {
     return locations;
   };
 
-const accesSToken = localStorage.getItem('access_token');
 // No Access Token Found in Local Storage
-const tokenCheck = accessToken && (await checkToken(accessToken));
+const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
 
-if (!accessToken || tokenCheck.error) {
-  await localStorage.removeItem('access_token');
-  const searchParams = new URLSearchParams(window.location.search);
-  const code = await searchParams.get('code');
-  if (!code) {
-    const results = await axios.get('https://b3pe6tfdal.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
-    const { authUrl } = results.data;
-    return (window.location.href = authUrl);
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code');
+    if (!code) {
+      const results = await axios.get('https://b3pe6tfdal.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
   }
-  return code && getToken(code);
+  return accessToken;
 }
-return accessToken;
 
 // Access Token Found in Local Storage
 const checkToken = async (accessToken) => {
@@ -63,7 +60,6 @@ export const getEvents = async () => {
   
 };
 
-  
   const removeQuery = () => {
     if (window.history.pushState && window.location.pathname) {
       var newurl = window.location.protocol + '//' + window.location.host + window.location.pathname;
@@ -74,18 +70,4 @@ export const getEvents = async () => {
     }
   };
 
-  const getToken = async (code) => {
-    try{
-      const encodedCode = encodeURIComponent(code);
-      const response = await fetch('https://b3pe6tfdal.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodedCode)
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-    }  
-      const { access_token } = await response.json();
-      access_token && localStorage.setItem('access_token', access_token);
-      return access_token;
-    } catch (error) {
-      error.json();
-    }
-    
-  };
+  
