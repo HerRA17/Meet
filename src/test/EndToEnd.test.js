dde7656d849334835e3ad2c51a20c197224441a6
Hermann Rasch <hermann.rasch93@gmail.com>
2023-05-10 21:29:38 +0200

    Adjustments to E2E but still with issues

--- src/test/EndToEnd.test.js
+++ src/test/EndToEnd.test.js
@@ -1,10 +1,15 @@
-import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer';
 describe('show/hide an event details', () => {
   let browser;
   let page;
+  
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 200,
+      ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+    });
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     await page.waitForSelector('.event');
@@ -25,5 +30,10 @@ describe('show/hide an event details', () => {
     expect(eventDetails).toBeDefined();
   });
 
+  test('User can collapse an event to hide its details', async () => {
+    await page.click('.event .details-btn');
+    const eventDetails = await page.$('.event .event__Details');
+    expect(eventDetails).toBeNull();
+  });
 });
 
