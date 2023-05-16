a841577a86405e3b7cd8a82f65aa90d93616ff7f
Hermann Rasch <hermann.rasch93@gmail.com>
2023-05-16 10:32:54 +0200

    Progress with testing

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -1,19 +1,31 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { mockData } from '../MockData';
+import App from '../App';
+import CitySearch from '../CitySearch';
+import Event from '../Event';
 
 const feature = loadFeature('./src/features/showHideAnEventsDetails.feature');
 
 defineFeature(feature, test => {
     test('An event element is collapse by default.', ({ given, when, then }) => {
-        given('user user has searched for a city', () => {
-
+        let AppWrapper, EventWrapper, CitySearchWrapper;
+        beforeEach(() => {
+            AppWrapper = mount(<App/>);
+            EventWrapper = AppWrapper.find(Event);
+            CitySearchWrapper = AppWrapper.find(CitySearch);
+        })
+        given('user has searched for a city', () => {
+            CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
         });
 
         when('the events are listed', () => {
-
+            ('suggestions li')
         });
 
         then('the event details should be collapsed by default', () => {
-
+            expect(EventWrapper.find('.EventDetails')).toHaveLength(mockData.length);
         });
     });
 
@@ -23,7 +35,8 @@ defineFeature(feature, test => {
         });
 
         when('a user clicks on the details button', () => {
-
+            EventWrapper.find('.detailsButton').simulate('change');
+                    
         });
 
         then('the event details should expand displaying its details', () => {
@@ -37,11 +50,11 @@ defineFeature(feature, test => {
         });
 
         when('user clicks collapse button', () => {
-
+            EventWrapper.find('.detailsButton').simulate('change');
         });
 
         then('the details should collapse within the specific event, hiding its details', () => {
-
+            expect(EventWrapper.find('')).toHaveLength(mockData);
         });
     });
 });
