1d724f98009de5f928ebed933a9f81857fbd2d69
Hermann Rasch <hermann.rasch93@gmail.com>
2023-05-16 10:32:48 +0200

    Progress with testing

--- src/features/specificNumberOfEvents.test.js
+++ src/features/specificNumberOfEvents.test.js
@@ -1,35 +1,47 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { mockData } from '../MockData';
+import App from '../App';
+import CitySearch from '../CitySearch';
+import Event from '../Event';
+import NumberOfEvents from '../NumberOfEvents';
 
 const feature = loadFeature('./src/features/specificNumberOfEvents.feature');
 
 defineFeature(feature, test => {
 
     test('When user has not specified a number, 32 is the default number.', ({ given, when, then }) => {
+        let AppWrapper, NumberOfEventsWrapper, CitySearchtWrapper;
+        beforeEach(() => {
+            const AppWrapper = mount(<App />);
+            NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+            CitySearchtWrapper = AppWrapper.find(CitySearch);
+        })
         given('a blank number of events field', () => {
-
-        });
+            });
 
         when('a user clicks search', () => {
-
+            NumberOfEventsWrapper.find('.number').simulate('change'); 
         });
 
         then('show the default number of events to be displayed', () => {
-
+            expect(NumberOfEventsWrapper.state('eventNumberResult')).toBeEqual(32);
         });
     });
 
     test('User can change the number of events they want to see.', ({ given, when, then }) => {
-        given('user can change the number of events', () => {
-
+        given('user can change the number of events', async () => {
+            
         });
 
         when('the user types within the number box', () => {
-
+           const event = { target: {value: 10} };
+           NumberOfEventsWrapper.find('.number').simulate('change', event);
         });
 
         then('the user should receive the provided amount of events', () => {
-
+            expect(NumberOfEventsWrapper.state('eventNumberResult')).toBeEqual(10);
         });
     });
-
 });
