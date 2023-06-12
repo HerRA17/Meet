9d79f55f30aa6d04e4d18432a96efd329a92ec1e
Hermann Rasch <hermann.rasch93@gmail.com>
2023-06-12 22:00:52 +0200

    reverting the file to a previous version that displays part of it

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,73 +1,73 @@
-import React, { Component } from 'react';
-import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+// const RADIAN = Math.PI / 180
+// const renderCustomizedLabel = ({
+//   cx,
+//   cy,
+//   midAngle,
+//   innerRadius,
+//   outerRadius,
+//   percent,
+// }) => {
+//   const radius = innerRadius + (outerRadius - innerRadius) * 0.5; //have a look at this part
+//   const x = cx + radius * Math.cos(-midAngle * RADIAN);
+//   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-  if (percent > 0) {
+//   if (percent > 0) {
+//     return (
+//       <text
+//         x={x}
+//         y={y}
+//         fill="white"
+//         textAnchor={x > cx ? 'start' : 'end'}
+//         dominantBaseline="central"
+//       >
+//         {`${(percent * 100).toFixed(0)}%`}
+//       </text>
+//     );
+//   } else {
+//     return null;
+//   }
+// };
+
+const EventGenre = ({ events }) => {
+    const [data, setData] = useState([]);
+    
+    const Colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#D4ADFC']
+
+    useEffect(() => {setData(() => getData()); }, [events]); //have a look at eslint-- exhaustive-deps
+
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+          const value = events.filter(({ summary }) => summary.split('').includes(genre)).length
+          return {name: genre , value};
+        })
+        
+        return data;
+    };
+    console.log(data);
+    console.log(events);
     return (
-      <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > cx ? 'start' : 'end'}
-        dominantBaseline="central"
-      >
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
+        <ResponsiveContainer height={400}>
+          <PieChart width={400} height={400}>
+            <Legend />
+            <Pie
+              data={data}
+              cx={200}
+              cy={200}
+              labelLine={false}
+              outerRadius={80}
+              dataKey="value"
+              label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+            >
+               {/* {data.map((entry, index) => (<Cell key={`cell-${index}`} fill={Colors[index]} /> ))} */}
+              
+             
+           </Pie>
+           </PieChart>
+        </ResponsiveContainer>
     );
-  } else {
-    return null;
-  }
-};
-
-class EventGenre extends Component {
-  getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-    const data = genres.map((genre) => {
-      const value = this.props.events.filter((event) => {
-        // .some() has to be added, so .includes() is not run on the array (returning only exact matches) but on every single array element
-        return event.summary.split(' ').some((summaryWord) => {
-          return summaryWord.includes(genre);
-        });
-      }).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
-
-  render() {
-    const colors = ['#2c3e50', '#27ae60', '#f39c12', '#808080', '#3498db'];
-
-    return (
-      <ResponsiveContainer className="eventGenre" height={300}>
-        <PieChart>
-          <Legend />
-          <Pie
-            data={this.getData()}
-            outerRadius={100}
-            dataKey="value"
-            label={renderCustomizedLabel}
-            labelLine={false}
-          >
-            {this.getData().map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
 }
-
-export default  EventGenre;
+     export default EventGenre;
