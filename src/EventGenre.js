b858c6c6e0663c2f93c10a16d6954f785536abdb
Hermann Rasch <hermann.rasch93@gmail.com>
2023-06-20 15:31:13 +0200

    Returning to previews set-up

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,37 +1,28 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, ResponsiveContainer, Legend, Cell } from 'recharts';
 
 const EventGenre = ({ events }) => {
     const [data, setData] = useState([]);
     
-    const Colors = ['#0088FE', '#00C49F', '#ECF8F9', '#D80303', '#D4ADFC']
+    const Colors = ['#0088FE', '#00C49F', '#FF8042', '#D80303', '#D4ADFC']
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const newData = await getData();
-        setData(newData);
-      }
-      fetchData();
-    }, [events]); //have a look at eslint-- exhaustive-deps
+  useEffect(() => { setData(() => getData()); }, [events]);   // eslint-disable-line react-hooks/exhaustive-deps
 
-    const getData = async () => {
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-      const newData = await Promise.all(
-        genres.map(async (genre) => {
-          const value = events.filter(({ summary }) => summary.split('').includes(genre)).length
-          return {name: genre , value};
-        })
-      );
-      
-      return newData;
-    };
-    console.log(data.value)
+  const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const data = genres.map((genre) => {
+      const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
+      return { name: genre, value };
+    });
+    return data;
+  };
+    console.log(data)
     return (
         <ResponsiveContainer height={400}>
           <PieChart width={400} height={400}>
             <Legend />
             <Pie
-              data={data}
+              data={getData()}
               cx={200}
               cy={200}
               labelLine={false}
